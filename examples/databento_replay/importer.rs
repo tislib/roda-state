b275578d4dd314ab64ab37cd9c0d467e215a9310
Taleh Ibrahimli <talehsmail@gmail.com>
2026-02-16 01:55:55 +0100

    fixes

--- examples/databento_replay/importer.rs
+++ examples/databento_replay/importer.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::path::PathBuf;
+use std::thread::sleep;
 use std::time::{Duration, Instant};
 
 use dbn::Record;
@@ -47,11 +48,12 @@ pub fn import_mbo_file(
 
                 if elapsed_market > elapsed_now {
                     let sleep_dur = Duration::from_nanos(elapsed_market - elapsed_now);
-                    if sleep_dur > Duration::from_secs(1) { // reset
+                    if sleep_dur > Duration::from_secs(1) {
+                        // reset
                         first_ts = None;
                         first_now = Instant::now();
                     } else if sleep_dur > Duration::from_micros(10) {
-                        std::thread::sleep(sleep_dur);
+                        sleep(Duration::from_micros(10));
                     }
                 }
             } else if count == 0 {
@@ -62,6 +64,11 @@ pub fn import_mbo_file(
             let ts_recv = crate::latency_tracker::get_relative_nanos();
             market_store.append(&LightMboEntry::from_msg(msg, ts_recv));
             count += 1;
+
+            if start.elapsed().as_secs() > 20 {
+                info!("[Writer] Stopped after 20 seconds...");
+                break;
+            }
         }
     }
 
